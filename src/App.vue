515f8f3fa59a6f0e5011dc8885b3f43484705e1a
Austen Ezzell <austenezzell@austens-mbp.lan>
2023-05-17 10:11:39 -0700

    added dialtone

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,6 @@ export default {
 </script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./index.less";
+@import "../node_modules/@dialpad/dialtone/lib/build/less/dialtone.less";
 </style>
